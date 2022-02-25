cc9a4f7f72ea787f4aa8914d0e45a62b2ee8636e
Iana Correa Reis <ianacorreahz@gmail.com>
2022-02-25 18:36:42 -0300

    implementação da funcionalidade de maximizar o modal

--- Fase-02/iniciando-no-front-end/scripts.js
+++ Fase-02/iniciando-no-front-end/scripts.js
@@ -1,15 +1,28 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
+const modal = document.querySelector('.modal');
 
 for (let card of cards) {
     card.addEventListener("click", function(){
         const postId = card.getAttribute("id");
         modalOverlay.classList.add('active')
         modalOverlay.querySelector("iframe").src = `https://blog.rocketseat.com.br/${postId}`;
+
+        if(modal.classList.contains('maximize')) {
+            modal.classList.remove('maximize');
+        }
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function(){
     modalOverlay.classList.remove("active")
     modalOverlay.querySelector("iframe").src = ""
+})
+
+document.querySelector('.maximize-modal').addEventListener('click', function() {
+    if(modal.classList.contains('maximize')) {
+        modal.classList.remove('maximize');
+    } else {
+        modal.classList.add('maximize');
+    }
 })
